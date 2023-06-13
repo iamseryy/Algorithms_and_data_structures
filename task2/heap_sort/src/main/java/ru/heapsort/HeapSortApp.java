4351aa892fa44fa5e64a6aa5385df273f3011293
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-06-13 08:58:05 +0300

    task2 ready

---
+++ task2/heap_sort/src/main/java/ru/heapsort/HeapSortApp.java
@@ -0,0 +1,12 @@
+package ru.heapsort;
+
+
+import ru.heapsort.controller.AppController;
+
+public class HeapSortApp
+{
+    public static void main( String[] args )
+    {
+        AppController.start();
+    }
+}
